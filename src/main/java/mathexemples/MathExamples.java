d5f794d9e43d26aed8aecfefca68b1a27b9d39eb
Keisy <k22.lizandra@gmail.com>
2024-07-01 08:51:27 -0300

    feat: Add BigDecimal and double addition methods to MathExamples class

--- src/main/java/mathexemples/MathExamples.java
+++ src/main/java/mathexemples/MathExamples.java
@@ -1,5 +1,6 @@
 package mathexemples;
-
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class MathExamples 
 {
@@ -7,12 +8,26 @@ public class MathExamples
     {
         int intNumA = 5;
         int intNumB = 3;
+        double dblNumC = 5.2d;
+        double dblNumD = 3.1d;
+        double dblNumE = 0.1f;
+        double dblNumF = 0.2f;
+
+        RoundingMode rmHalfUp = RoundingMode.HALF_UP;
+        BigDecimal bdNumE = new BigDecimal (0.1).setScale(1, rmHalfUp);
+        BigDecimal bdNumF = new BigDecimal (0.2).setScale(1, rmHalfUp);
+
+
 
         System.out.println(intNumA + " + " +  intNumB + " = " + add(intNumA,intNumB));
         System.out.println(intNumA + " x " + intNumB + " = "+ multiply(intNumA,intNumB));
         System.out.println(intNumA +  " - " + intNumB + " = " + subtract(intNumA,intNumB));
         System.out.println(intNumA +  " mod " + intNumB + " = " + modulo(intNumA,intNumB));
         System.out.println(intNumA +  " to the power of " + intNumB + " = " + exponent(intNumA,intNumB));
+        System.out.println(dblNumC + " + " + dblNumD + " = " + add(dblNumC,dblNumD));
+        System.out.println(dblNumE + " + " + dblNumF + " = " + add(dblNumE, dblNumF));
+        System.out.println((0.1f == 0.1d));
+        System.out.println(bdNumE + " + " + bdNumF + " = " + add(bdNumE, bdNumF));
     }
 
     public static int add(int intNum1, int intNum2) {
@@ -35,4 +50,11 @@ public class MathExamples
         return (int) Math.pow(base, power);
     }
     
+    public static BigDecimal add(BigDecimal bdNum1, BigDecimal bdNum2) {
+        return bdNum1.add(bdNum2);
+    }
+
+    public static Double add(double dblNum1, double dblNum2) {
+        return dblNum1 + dblNum2;
+    }
 }
